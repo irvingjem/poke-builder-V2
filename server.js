9e189564af6d03bd1a656626a377a8616b321d7c
Kaia Scheirman <kfscheirman18@gmail.com>
2022-05-05 15:52:53 -0700

    set up server to use the handlebars engine

--- server.js
+++ server.js
@@ -17,6 +17,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// handlebars
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 // Sequelize
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => {
