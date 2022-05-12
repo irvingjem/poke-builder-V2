9bd5461f4304d33775250cd934647fcf1655ebe3
Kaia Scheirman <kfscheirman18@gmail.com>
2022-05-11 17:14:55 -0700

    create route that returns a single user based off id

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -9,7 +9,27 @@ router.get('/', (req, res) => {
     })
     .then(userData => res.json(userData))
     .catch(err => {
-        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// get single user based off id
+router.get('/:id', (req, res) => {
+    User.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: { exclude: ['password'] }
+        // add code for pokemon team
+    })
+    .then(userData => {
+        if(!userData) {
+            res.status(404).json({ message: 'No trainer found with this ID.' });
+            return;
+        }
+        res.json(userData);
+    })
+    .catch(err => {
         res.status(500).json(err);
     });
 });
