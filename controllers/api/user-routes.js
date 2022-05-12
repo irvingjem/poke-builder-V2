6c880b4c1ad217757934aa0887908ae50a518243
Kaia Scheirman <kfscheirman18@gmail.com>
2022-05-11 17:30:22 -0700

    create route for logout

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -83,4 +83,15 @@ router.post('/login', (req, res) => {
     });
 });
 
+// logout route
+router.post('/logout', (req, res) => {
+    if(req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
+
 module.exports = router;
