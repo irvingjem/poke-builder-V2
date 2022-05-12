aa5b5b3802b78e115713f30edb5df0131e89d391
superfishal <97133716+superfishal@users.noreply.github.com>
2022-05-11 21:58:52 -0700

    generate/save working - file/folder cleanup

--- public/javascript/generate.js
+++ public/javascript/generate.js
@@ -1,85 +1,94 @@
-var abilitiesList = document.getElementById("abilities");
+let abilitiesList = document.getElementById("abilities");
 let pokeDiv = document.getElementById("pokeDetails");
-var submitListener = document.getElementById("submitListener");
-
-// Grab pokemon name / and image
-// //After installing pokepromise(or whatever they call it) grab info from npm package
-// router.get('/:pokeid', (req, res) => {    
-
-//     //Grab pokemon name from URL params
-//     P.getPokemonByName(req.params.pokeid)
-//     //response.JSON
-//     .then((response) => {
-//         res.send(response);
-//     })
-//     //If catch any errors a cheeky response is given
-//     .catch((error) => {
-//         console.log('Whoa, thats not supposed to happen: ', error);
-//     })
-
-// });
-// //END
-
-
-
+let submitListener = document.getElementById("submitListener");
+let globalPokemonName;
 function resetScreen() {
-    //need to fill
-    abilities.innerHTML = "";
+  //need to fill
+  abilities.innerHTML = "";
 }
+// Fetch Pokemon data
+let fetchPokeData = function (pokeName) {
+  let pokeNameURL =
+    "https://pokeapi.co/api/v2/pokemon/" + pokeName.toLowerCase();
+  fetch(pokeNameURL)
+    .then(function (pokeNameResponse) {
+      return pokeNameResponse.json();
+    })
+    .then(function (pokeNameData) {
+      console.log("pokeNameData:", pokeNameData.name);
+      globalPokemonName = pokeNameData.name.toLowerCase();
+      resetScreen();
+
+      // Save local to storage
+      // savePokemon(pokeNameData.name);
+      //setting up the character name
+      let dataTypes = pokeNameData["name"];
+      document.getElementById("pokename").textContent =
+        "Name: " + dataTypes[0].toUpperCase() + dataTypes.substring(1);
+      //Calling the ability fetch based on the character name data
+      fetchPokeAbility(dataTypes);
+
+      let imgElement = document.getElementById("charcterImg");
+      console.log("charcterImgURL:", pokeNameData.sprites.front_default);
+      imgElement.setAttribute("src", pokeNameData.sprites.front_default);
+      imgElement.setAttribute("alt", dataTypes);
+    });
+};
 // Display Name Function -
-let getPokeName = function() {
-    pokeDiv.classList.add("font-bold", "text-xl", "mb-2");
-    var pTag = document.createElement("p");
-    pTag.setAttribute("id", "pokename");
-    pTag.textContent = "Name: ";
-    pokeDiv.append(pTag);
+let getPokeName = function () {
+  pokeDiv.classList.add("font-bold", "text-xl", "mb-2");
+  let pTag = document.createElement("p");
+  pTag.setAttribute("id", "pokename");
+  pTag.textContent = "Name: ";
+  pokeDiv.append(pTag);
 };
 // Fetch Abilities Function -- Jem
-
-var fetchPokeAbility = function(pokeName) {
-    var pokeAbilityURL = "https://pokeapi.co/api/v2/pokemon/" + pokeName;
-    fetch(pokeAbilityURL)
-        .then(function(pokeAbilityResponse) {
-            return pokeAbilityResponse.json();
-        })
-        .then(function(pokeAbilityData) {
-            var dataAbility = pokeAbilityData.abilities;
-            console.log("pokeAbilityData", pokeAbilityData.abilities);
-            var dataFirstAbility = dataAbility[0];
-            var dataSecondAbility = dataAbility[1];
-            var dataThirdAbility = dataAbility[2];
-            console.log("first", dataFirstAbility, "second", dataSecondAbility);
-            //createElement
-            var liTag = document.createElement("li");
-            liTag.textContent =
-                // a lot of code to capitalize the first letter
-                dataFirstAbility.ability.name.charAt(0).toUpperCase() +
-                dataFirstAbility.ability.name.slice(1);
-            //append it to ul List
-            abilitiesList.append(liTag);
-            //createElement
-            var liTag2 = document.createElement("li");
-            liTag2.textContent =
-                // a lot of code to capitalize the first letter
-                dataSecondAbility.ability.name.charAt(0).toUpperCase() +
-                dataSecondAbility.ability.name.slice(1);
-            //append it to ul List
-            abilitiesList.append(liTag2);
-            //createElement
-            var liTag3 = document.createElement("li");
-            liTag3.textContent =
-                // a lot of code to capitalize the first letter
-                dataThirdAbility.ability.name.charAt(0).toUpperCase() +
-                dataThirdAbility.ability.name.slice(1);
-            //append it to ul List
-            abilitiesList.append(liTag3);
-        });
+const fetchPokeAbility = function (pokeName) {
+  let pokeAbilityURL = "https://pokeapi.co/api/v2/pokemon/" + pokeName;
+  fetch(pokeAbilityURL)
+    .then(function (pokeAbilityResponse) {
+      return pokeAbilityResponse.json();
+    })
+    .then(function (pokeAbilityData) {
+      let dataAbility = pokeAbilityData.abilities;
+      let dataFirstAbility = dataAbility[0];
+      let dataSecondAbility = dataAbility[1];
+      let dataThirdAbility = dataAbility[2];
+      //createElement
+      let liTag = document.createElement("li");
+      liTag.textContent =
+        // a lot of code to capitalize the first letter
+        dataFirstAbility.ability.name.charAt(0).toUpperCase() +
+        dataFirstAbility.ability.name.slice(1);
+      //append it to ul List
+      abilitiesList.append(liTag);
+      //createElement
+      let liTag2 = document.createElement("li");
+      liTag2.textContent =
+        // a lot of code to capitalize the first letter
+        dataSecondAbility.ability.name.charAt(0).toUpperCase() +
+        dataSecondAbility.ability.name.slice(1);
+      //append it to ul List
+      abilitiesList.append(liTag2);
+      //createElement
+      let liTag3 = document.createElement("li");
+      liTag3.textContent =
+        // a lot of code to capitalize the first letter
+        dataThirdAbility.ability.name.charAt(0).toUpperCase() +
+        dataThirdAbility.ability.name.slice(1);
+      //append it to ul List
+      abilitiesList.append(liTag3);
+    });
 };
+
 getPokeName();
 
-submitListener.addEventListener("submit", function(event) {
-    event.preventDefault();
-    // var searchText = document.getElementById("characterName").value;
-    var showData = document.getElementById("invisible");
-    showData.classList.remove("invisible");
+submitListener.addEventListener("click", function (event) {
+  event.preventDefault();
+  console.log(event);
+  let searchText = document.getElementById("characterName").value;
+  let showData = document.getElementById("invisible");
+  showData.classList.remove("invisible");
+  fetchPokeData(searchText);
+  characterName.value = "";
 });
---
+++ public/javascript/save.js
@@ -0,0 +1,7 @@
+let saveListener = document.getElementById("saveListener");
+
+saveListener.addEventListener("click", function (event) {
+  event.preventDefault();
+  // holy fuck i got the number
+  console.log(globalPokemonName);
+});
