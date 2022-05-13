ea7d22f81b077ce88a44826131240f3ed37cce14
Your Name <irving.jem@gmail.com>
2022-05-12 21:18:17 -0700

    save functionality working :)

--- public/javascript/save.js
+++ public/javascript/save.js
@@ -2,13 +2,13 @@ let saveListener = document.getElementById("saveListener");
 
 saveListener.addEventListener("click", function(event) {
     event.preventDefault();
-
+    // fetch to save name 
     fetch("/api/users/pokemon", {
         method: 'put',
         body: JSON.stringify({ pokeId: globalPokemonName }),
         headers: {
             'Content-Type': 'application/json'
-        }
-    }).then(() => document.location.replace("/generate"))
+        } // user save redirect
+    }).then(() => document.location.replace("/generate/"))
     console.log(globalPokemonName);
 });
